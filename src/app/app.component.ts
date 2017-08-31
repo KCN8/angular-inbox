840025778468814c12391ee6b167118df31e3f37
Casey Herold <caseynate@gmail.com>
2017-08-31 14:15:36 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import data from './app.data'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  messages = data
+
+  togglestar(id) {
+    for(let i = 0; i < this.messages.length; i++) {
+      if (id === this.messages[i].id) {
+        this.messages[i].starred = !this.messages[i].starred
+      }
+    }
+  }
+
+  togglecheckbox(id) {
+    for(let i = 0; i < this.messages.length; i++) {
+      if (id === this.messages[i].id) {
+        this.messages[i].selected = !this.messages[i].selected
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageComponent } from './messages/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule
